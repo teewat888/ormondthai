a9ce9ab52da7327c536361621c2c654662b450c8
Teerawat P <teewat@yahoo.com>
2025-08-29 12:43:48 +1000

    update title and script source in index.html; add coming soon page with styles and content in comingsoon.css and comingsoon.ts

---
+++ src/comingsoon.ts
@@ -0,0 +1,9 @@
+import './comingsoon.css';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="coming-soon">
+    <h1>Coming Soon</h1>
+    <p>Our delicious website is almost ready to be served!</p>
+    <i class="fas fa-utensils"></i>
+  </div>
+`;
